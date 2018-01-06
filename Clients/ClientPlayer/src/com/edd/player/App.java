19bf77bdeed09481be069d53762f8ba01d08863f
renatosierra <renato.wellmann@gmail.com>
2018-01-06 03:01:19 -0600

    merge

--- Clients/ClientPlayer/src/com/edd/player/App.java
+++ Clients/ClientPlayer/src/com/edd/player/App.java
@@ -3,11 +3,11 @@ package com.edd.player;
 import com.edd.player.DTO.DTOLogin;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 public class App extends JFrame{
@@ -18,17 +18,21 @@ public class App extends JFrame{
     private JList list3;
     private JList list4;
     private JList list5;
-    private JPanel panel1 = new JPanel(null);
+    private JPanel panel1;
     private JButton button1;
     DefaultListModel model;
 
+    public DefaultListModel getModel() {
+        return model;
+    }
+
+    public void setModel(DefaultListModel model) {
+        this.model = model;
+    }
 
 
 
-    public App( ) {
-
-
-
+    public App() {
         this.setContentPane(this.jpContent);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(this.MAXIMIZED_BOTH);
@@ -36,7 +40,7 @@ public class App extends JFrame{
         this.setVisible(true);
 
 
-
+        panel1.setVisible(true);
 
         JFrame frame = new JFrame("App");
 
@@ -52,53 +56,25 @@ public class App extends JFrame{
         dialog.setSize(400,200);
         dialog.setLocationRelativeTo(null);
 
-        DefaultListModel listModel1 = new DefaultListModel();
-        list1.setModel(listModel1);
-        DefaultListModel listModel2 = new DefaultListModel();
-        list2.setModel(listModel2);
-        DefaultListModel listModel3 = new DefaultListModel();
-        list3.setModel(listModel3);
-        DefaultListModel listModel4 = new DefaultListModel();
-        list4.setModel(listModel4);
-        DefaultListModel listModel5 = new DefaultListModel();
-        list5.setModel(listModel5);
-
         dialog.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosed(WindowEvent e) {
                 DTOLogin dto = dialog.getData();
                 for(com.edd.player.DTO.DTOLogin.Data.Year year : dto.data.getYears()){
                     App.this.getModel().addElement(year);
-
-                    ((DefaultListModel)list1.getModel()).addElement(year);
                 }
             }
         });
         dialog.setVisible(true);
-        list1.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                //com.edd.player.DTO.DTOLogin.Data.Year year = (com.edd.player.DTO.DTOLogin.Data.Year)e;
-
-                com.edd.player.DTO.DTOLogin.Data.Year year = null;
-
-                super.mouseClicked(e);
-            }
-        });
-        list1.addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-
-                DTOLogin.Data.Year selectedYear = (DTOLogin.Data.Year) list1.getModel().getElementAt(list1.getSelectedIndex());
-
-                //System.out.println(sel);
-            }
-        });
     }
     public static void main(String[] args){
 
 
-        new App();
+      //  new App();
+
+        Reproductor ventana = new Reproductor();
+
+        ventana.setVisible(true);
 
 
 
