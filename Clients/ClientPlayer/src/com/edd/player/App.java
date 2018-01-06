73b7a94225f7516572b7024c94267be372367a46
renatosierra <renato.wellmann@gmail.com>
2018-01-05 18:42:22 -0600

    Login con web service hacia flask

--- Clients/ClientPlayer/src/com/edd/player/App.java
+++ Clients/ClientPlayer/src/com/edd/player/App.java
@@ -3,11 +3,10 @@ package com.edd.player;
 import com.edd.player.DTO.DTOLogin;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 
 public class App extends JFrame{
@@ -18,40 +17,57 @@ public class App extends JFrame{
     private JList list3;
     private JList list4;
     private JList list5;
-    DefaultListModel model;
 
-    public DefaultListModel getModel() {
-        return model;
-    }
-
-    public void setModel(DefaultListModel model) {
-        this.model = model;
-    }
 
 
 
     public App() {
 
-        JFrame frame = new JFrame("App");
-
-
-        DefaultListModel model = new DefaultListModel();
-        list1.setModel(model);
         LoginDialog dialog = new LoginDialog();
 
         dialog.setSize(400,200);
         dialog.setLocationRelativeTo(null);
+
+        DefaultListModel listModel1 = new DefaultListModel();
+        list1.setModel(listModel1);
+        DefaultListModel listModel2 = new DefaultListModel();
+        list2.setModel(listModel2);
+        DefaultListModel listModel3 = new DefaultListModel();
+        list3.setModel(listModel3);
+        DefaultListModel listModel4 = new DefaultListModel();
+        list4.setModel(listModel4);
+        DefaultListModel listModel5 = new DefaultListModel();
+        list5.setModel(listModel5);
 
         dialog.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosed(WindowEvent e) {
                 DTOLogin dto = dialog.getData();
                 for(com.edd.player.DTO.DTOLogin.Data.Year year : dto.data.getYears()){
-                    App.this.getModel().addElement(year);
+                    ((DefaultListModel)list1.getModel()).addElement(year);
                 }
             }
         });
         dialog.setVisible(true);
+        list1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //com.edd.player.DTO.DTOLogin.Data.Year year = (com.edd.player.DTO.DTOLogin.Data.Year)e;
+
+                com.edd.player.DTO.DTOLogin.Data.Year year = null;
+
+                super.mouseClicked(e);
+            }
+        });
+        list1.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+
+                DTOLogin.Data.Year selectedYear = (DTOLogin.Data.Year) list1.getModel().getElementAt(list1.getSelectedIndex());
+
+                //System.out.println(sel);
+            }
+        });
     }
     public static void main(String[] args){
 
@@ -59,7 +75,11 @@ public class App extends JFrame{
         myApp.setContentPane(myApp.jpContent);
         myApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myApp.setExtendedState(myApp.MAXIMIZED_BOTH);
-        myApp.pack();
         myApp.setVisible(true);
+        myApp.pack();
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
